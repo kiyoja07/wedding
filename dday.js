fedcaf3a2d21d5b2aa275968628a7daffbb9a02d
kioyoja07 <kiyoja07@gmail.com>
2019-03-03 22:45:47 +0900

    adjust d day calculator

--- dday.js
+++ dday.js
@@ -4,13 +4,21 @@ const dDay = new Date("2019-06-15");
 const now = new Date(); 
 
 function displayResult(gapDay) {
-    const reverseResult = gapDay * -1;
-    Day.innerText = `D${reverseResult}`;
+    const reverseResult = Math.abs(gapDay);
+
+    if (gapDay > 0) {
+        Day.innerText = `D - ${reverseResult}`;
+    } else if (gapDay < 0) {
+        Day.innerText = `D + ${reverseResult}`;
+    } else {
+        Day.innerText = `D - Day`;
+    }
 }
 
 function dayCalculator() {
     const gapTime = dDay.getTime() - now.getTime();
-    const gapDay = Math.floor(gapTime / (1000 * 60 * 60 * 24));
+    const gapDay = Math.floor(gapTime / (1000 * 60 * 60 * 24)) + 1;
+    console.log(gapDay)
     displayResult(gapDay);
 }
 
